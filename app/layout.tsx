c001e3872fa8fdab84f232921ceed8b51902d891
specialcowboy69 <franciscoestangui@yahoo.es>
2026-08-06 14:05:05 +0200

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 ﻿import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,6 +37,15 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>{children}</body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-VECVHEZ2DN" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VECVHEZ2DN');
+        `}
+      </Script>
     </html>
   );
 }
